3a272be81e2a18692de36ae76bee0f1750c90d79
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2017-12-23 19:15:59 -0500

    New expert traces and results from running cbr test

--- src/PerformanceTesting/PerformanceTest.java
+++ src/PerformanceTesting/PerformanceTest.java
@@ -12,13 +12,13 @@ public class PerformanceTest extends PerformanceEvaluator {
 	
 	public static void main(String a[]) throws IOException{
 		//String [] filenames = {"Data/Carleton_1.lsf","Data/Carleton_2.lsf","Data/Carleton_3.lsf","Data/Carleton_4.lsf","Data/Carleton_5.lsf","Data/University_1.lsf","Data/University_2.lsf","Data/University_3.lsf","Data/University_4.lsf","Data/University_5.lsf"};
-		String [] filenames = {"Data/file1.txt","Data/file2.txt"};
-		String output_filename = "Results/ctsmodel,cbr,none,none,none,none,.csv";
+		String [] filenames = {"Data/file0.txt","Data/file1.txt","Data/file2.txt","Data/file3.txt","Data/file4.txt","Data/file5.txt","Data/file6.txt","Data/file7.txt","Data/file8.txt","Data/file9.txt"};
+		String output_filename = "Results/ctsmodel,bayesian,none,none,none,none,.csv";
 	
 		//CaseBaseFilter smote = new UnderSampling(standardize);
 		//CaseBaseFilter sample = new Sampling(standardize);
 		PerformanceTest pt = new PerformanceTest();
-		pt.PerformanceEvaluatorMethod(filenames, null, output_filename,"weightedKNN",null, null);
+		pt.PerformanceEvaluatorMethod(filenames, null, output_filename,"bayesian",null, null);
 	}
 
 	@Override
